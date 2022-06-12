298ec8398a5eee73d2c52a61c424cdc5d920ffb6
Orealba <orealbas@gmail.com>
2022-06-12 18:24:14 +0000

    creación menú hamburguesa

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,61 +1,76 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "../component../../../styles/navbar.css";
+import "../../styles/navbar.css";
+import logo from "../../img/Logo_amarillo.png";
 
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export const Navbar = () => {
+  function handleClick(e) {
+    e.preventDefault();
+  }
+
   return (
     <div>
-      <nav className="navbar navbar-expand-lg ">
+      <nav className="navbar navbar-expand-lg home-navbar">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            LOGO
-          </a>
-
-          <div className="collapse navbar-collapse navbar-buttons">
+          <div class="order-0">
+            <a className="navbar-brand" href="">
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target=".dual-collapse2"
+                onClick={handleClick.bind(this)}
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <img src={logo} className="navbar-logo" />
+            </a>
+          </div>
+          <div class="navbar-collapse collapse w-100 order-1 order-lg-1 dual-collapse2">
             <ul className="navbar-nav me-auto">
-              <li className="navbar-interior-button">
-                <a className="nav-link navbar-interior-button-2" href="#">
+              <li>
+                <a className="nav-link navbar-button" href="#">
                   Interior
                 </a>
               </li>
-              <li className="navbar-exterior-button">
-                <a className="nav-link navbar-exterior-button2" href="#">
+              <li>
+                <a className="nav-link navbar-button" href="#">
                   Exterior
                 </a>
               </li>
-              <li className="navbar-suculentas-button">
-                <a className="nav-link navbar-suculentas-button2" href="#">
+              <li>
+                <a className="nav-link navbar-button" href="#">
                   Suculentas
                 </a>
               </li>
-              <li className="navbar-cuidados-button2">
-                <a className="nav-link navbar-cuidados-button" href="#">
+              <li>
+                <a className="nav-link navbar-button" href="#">
                   Cuidados
                 </a>
               </li>
-              <li className="navbar-nosotros-button">
-                <a className="nav-link navbar-nosotros-button2" href="#">
+              <li>
+                <a className="nav-link navbar-button" href="#">
                   Nosotros
                 </a>
               </li>
             </ul>
-            <form class="form-search" method="get" id="s" action="/">
-              <div class="input-append">
-                <input
-                  type="text"
-                  class="input-medium search-query"
-                  name="s"
-                  placeholder="Search"
-                  value=""
-                />
-                <button type="submit" class="add-on nabvar-icon-search">
-                  <FontAwesomeIcon icon={faMagnifyingGlass} />
-                </button>
-              </div>
-            </form>
+          </div>
+          <div class="navbar-collapse collapse w-100 order-3">
+            <ul class="navbar-nav ms-auto">
+              <li>
+                <div class="navbar-search-button">
+                  <input
+                    type="text"
+                    placeholder="Buscar"
+                    class="form-control"
+                  />
+                  <i class="fa fa-search"></i>
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
