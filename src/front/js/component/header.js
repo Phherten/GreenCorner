4ae1c7e6ed7e81f9cb9f276a9dacf0f714805938
Orealba <orealbas@gmail.com>
2022-06-13 14:57:03 +0000

    creación de header

---
+++ src/front/js/component/header.js
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/header.css";
+import fondo from "../../img/header_background.jpg";
+
+export const Header = () => {
+  const { store, actions } = useContext(Context);
+
+  return (
+    <div className="container-fluid header-background">
+      <div>
+        <img src={fondo} className="header-background-img" />
+        <h2 className="header-title">Protege lo que te cuida</h2>
+        <h4 className="header-paragrap">
+          Nuestro mundo siempre ha sido verde, intentemos expandir este color.
+          <br />
+          Por eso te ayudamos a cuidar tus plantas,
+          <br />
+          porque un hogar lleno de plantas es un lugar lleno de vida.
+        </h4>
+        <button type="button" class="btn btn-primary">
+          Más información
+        </button>
+      </div>
+    </div>
+  );
+};
