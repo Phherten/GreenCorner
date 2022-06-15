4845e175376082ebbca326d6fb4c1aaa8201bb6f
barbosa92 <alexrp_92@hotmail.com>
2022-06-15 19:26:19 +0000

    User registration & token creation

--- src/api/routes.py
+++ src/api/routes.py
@@ -3,7 +3,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
 
-from api.models import db, Plagas, InfoPlant
+from api.models import db, Plagas, InfoPlant, User
 
 
 from api.utils import generate_sitemap, APIException
