daeb8435f42bb81c1e2ad2225ffebd3972991be1
Orealba <orealbas@gmail.com>
2022-06-15 15:39:33 +0000

    creación parte final media page

--- src/front/js/component/homeMediaPage.js
+++ src/front/js/component/homeMediaPage.js
@@ -3,9 +3,17 @@ import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 
 import "../../styles/homeMediaPage.css";
+import lineas from "../../img/líneas1.png";
+import lineaPequeña from "../../img/foto_plantas_media_pag.jpg";
 import plantasMediaPage from "../../img/foto_plantas_media_pag.jpg";
 import plantaInteriorHomeTarjeta from "../../img/planta_tarjeta_home_interior.png";
 import plantaRedondaExterior from "../../img/bola_planta_exterior.png";
+import plantaSuculenta from "../../img/suculentas.png";
+import hojaSueltas from "../../img/Hojas_sueltas.png";
+import lineaDos from "../../img/líneas2.png";
+import florMaceta from "../../img/flormaceta.png";
+import plagas from "../../img/plagas.png";
+import regadera from "../../img/regadera.png";
 
 export const HomeMediaPage = () => {
   const { store, actions } = useContext(Context);
@@ -16,7 +24,9 @@ export const HomeMediaPage = () => {
         <div className="col-md-4 home-col-media-page-photo">
           <img src={plantasMediaPage} className="media-page-plantas" />
         </div>
-        <div className="col-md-1"></div>
+        <div className="col-md-1">
+          <img src={lineas} className="media-page-linea-larga" />
+        </div>
         <div className="col-md-2">
           <div class="card-home-col-media-page-card-interior">
             <img
@@ -43,17 +53,75 @@ export const HomeMediaPage = () => {
               />
             </div>
           </div>
+          <div class="row">
+            <div class="col-md-8 card-home-col-media-page-card-suculentas">
+              <p class="media-page-card-text-suculentas">SUCULENTAS</p>{" "}
+            </div>
+            <div className="col-md-4">
+              <img
+                src={plantaSuculenta}
+                className="media-page-planta-tarjetas-suculentas-foto"
+              />
+            </div>
+          </div>
         </div>
         <div className="col-md-1"></div>
       </div>
-      <div className="row home-col-media-page-dark">
-        <div className="col-md-1"></div>
-        <div className="col-md-2"></div>
-        <div className="col-md-1"></div>
-        <div className="col-md-4"></div>
-        <div className="col-md-1"></div>
-        <div className="col-md-2"></div>
-        <div className="col-md-1"></div>
+      <div className="home-col-media-page-dark">
+        <img src={hojaSueltas} className="media-page-planta-suelta" />
+        <div>
+          <img src={lineaDos} className="media-page-lineas-larga-blancas" />
+
+          <h4 className="media-page-text">
+            Encuentra información sobre tus plantas
+          </h4>
+        </div>
+
+        <div className="row">
+          <div className="col-md-1"></div>
+          <div className="col-md-2">
+            <div class="card-home-media-page-card-cuidado-plantas">
+              <div class="card-body">
+                <p class="media-page-card-cuidados-plagas-notificaciones">
+                  CUIDADO DE PLANTAS
+                </p>
+              </div>
+              <img
+                src={florMaceta}
+                className="media-page-planta-tarjetas-cuidados-foto"
+              />
+            </div>
+          </div>
+          <div className="col-md-1"></div>
+          <div className="col-md-4">
+            <div class="card-home-media-page-card-plagas">
+              <div class="card-body">
+                <p class="media-page-card-cuidados-plagas-notificaciones">
+                  PLAGAS
+                </p>
+              </div>
+            </div>
+            <img
+              src={plagas}
+              className="media-page-planta-tarjetas-plagas-foto"
+            />
+          </div>
+          <div className="col-md-1"></div>
+          <div className="col-md-2">
+            <div class="card-home-media-page-card-cuidado-plantas">
+              <div class="card-body">
+                <p class="media-page-card-cuidados-plagas-notificaciones">
+                  NOTIFICACIONES DE RIEGO
+                </p>
+              </div>
+              <img
+                src={regadera}
+                className="media-page-planta-tarjetas-riego-foto"
+              />
+            </div>
+          </div>
+          <div className="col-md-1"></div>
+        </div>
       </div>
     </div>
   );
