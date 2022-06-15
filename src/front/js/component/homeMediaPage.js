d133a41672bd7726d20cde5e085f19314a6c0e2e
Orealba <orealbas@gmail.com>
2022-06-15 11:39:04 +0000

    modificación de tarjetas de media page

--- src/front/js/component/homeMediaPage.js
+++ src/front/js/component/homeMediaPage.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import "../../styles/homeMediaPage.css";
 import plantasMediaPage from "../../img/foto_plantas_media_pag.jpg";
 import plantaInteriorHomeTarjeta from "../../img/planta_tarjeta_home_interior.png";
+import plantaRedondaExterior from "../../img/bola_planta_exterior.png";
 
 export const HomeMediaPage = () => {
   const { store, actions } = useContext(Context);
@@ -13,41 +14,47 @@ export const HomeMediaPage = () => {
     <div className="container-fluid home-media-page">
       <div className="row home-col-media-page-white">
         <div className="col-md-4 home-col-media-page-photo">
-          <hr className="media-page-line-h"></hr>
-
           <img src={plantasMediaPage} className="media-page-plantas" />
         </div>
         <div className="col-md-1"></div>
         <div className="col-md-2">
-          <div class="card home-col-media-page-card-one">
+          <div class="card-home-col-media-page-card-interior">
             <img
               src={plantaInteriorHomeTarjeta}
               className="media-page-planta-tarjetas-interior-foto"
             />
             <div class="card-body">
-              <p class="media-page-card-text-interior">Interior</p>
+              <p class="media-page-card-text-interior">INTERIOR</p>
             </div>
           </div>
         </div>
+
         <div className="col-md-1"></div>
+
         <div className="col-md-3">
-          <div class="card">
-            <img src="..." class="card-img-top" alt="..." />
-            <div class="card-body">
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
+          <div class="card mb-3 card-home-col-media-page-card-exterior">
+            <div class="row">
+              <div class="col-md-12">
+                <div>
+                  <p class="media-page-card-text-exterior">EXTERIOR</p>{" "}
+                </div>
+              </div>
             </div>
-          </div>
-          <div class="card">
-            <img src="..." class="card-img-top" alt="..." />
-            <div class="card-body">
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
+            <img
+              src={plantaRedondaExterior}
+              className="media-page-planta-tarjetas-exterior-foto"
+            />
+            <div class="row">
+              <div class="col-md-12">
+                <div>
+                  <p class="media-page-card-text-suculentas">SUCULENTAS</p>{" "}
+                </div>
+              </div>
             </div>
+            <img
+              src={plantaRedondaExterior}
+              className="media-page-planta-tarjetas-exterior-foto"
+            />
           </div>
         </div>
         <div className="col-md-1"></div>
