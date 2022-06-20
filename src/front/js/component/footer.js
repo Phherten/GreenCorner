4ae5cc3ae8a54a77618598b751e2720ed165ee3f
Orealba <orealbas@gmail.com>
2022-06-20 17:06:36 +0000

    footer con redes sociales

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -4,54 +4,54 @@ import logoFooter from "../../img/Logo_letras_amarillo.png";
 
 export const Footer = () => {
   return (
-    <div class="container-fluid footer-home">
-      <section class="">
-        <footer class="text-center text-white">
-          <div class="container p-4 pb-0">
-            <section class="">
-              <div class="d-flex justify-content-center align-items-center">
+    <div className="container-fluid footer-home">
+      <section className="">
+        <footer className="text-center text-white">
+          <div className="container p-4 pb-0">
+            <section className="">
+              <div className="d-flex justify-content-center align-items-center">
                 <img className="header-logoLetrasAmarillo" src={logoFooter} />
               </div>
             </section>
           </div>
 
-          <div class="text-center p-3 footer-text">
+          <div className="text-center p-3 footer-text">
             © 2020 Copyright:
             <a className="text-footer-web" href="https://4geeksacademy.com/">
               4Geeks Academy
             </a>
           </div>
-          <section class="mb-4 text-center">
+          <section className="mb-4 text-center">
             <a
-              class="btn btn-outline-light btn-floating m-1"
+              className="btn btn-outline-light btn-floating m-1"
               href="#!"
               role="button"
             >
-              <i class="fab fa-facebook-f"></i>
+              <i className="fab fa-facebook-f"></i>
             </a>
 
             <a
-              class="btn btn-outline-light btn-floating m-1"
+              className="btn btn-outline-light btn-floating m-1"
               href="#!"
               role="button"
             >
-              <i class="fab fa-twitter"></i>
+              <i className="fab fa-twitter"></i>
             </a>
 
             <a
-              class="btn btn-outline-light btn-floating m-1"
+              className="btn btn-outline-light btn-floating m-1"
               href="#!"
               role="button"
             >
-              <i class="fab fa-instagram"></i>
+              <i className="fab fa-instagram"></i>
             </a>
 
             <a
-              class="btn btn-outline-light btn-floating m-1"
+              className="btn btn-outline-light btn-floating m-1"
               href="#!"
               role="button"
             >
-              <i class="fab fa-linkedin-in"></i>
+              <i className="fab fa-linkedin-in"></i>
             </a>
           </section>
         </footer>
