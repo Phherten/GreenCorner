b15841fe0604a645cd3d26ee74f09bfe8f39bbe4
Orealba <orealbas@gmail.com>
2022-06-20 12:18:38 +0000

    creacion footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
+import "../../styles/footer.css";
+import logoFooter from "../../img/Logo_letras_amarillo.png";
 
-export const Footer = () => (
-  <footer className="footer mt-auto py-3 text-center">
-    <p>
-      Made with <i className="fa fa-heart text-danger" /> by{" "}
-      <a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-    </p>
-  </footer>
-);
+export const Footer = () => {
+  return (
+    <div class="container-fluid footer-home">
+      <section class="">
+        <footer class="text-center text-white">
+          <div class="container p-4 pb-0">
+            <section class="">
+              <div class="d-flex justify-content-center align-items-center">
+                <img className="header-logoLetrasAmarillo" src={logoFooter} />
+              </div>
+            </section>
+          </div>
+
+          <div class="text-center p-3 footer-text">
+            © 2020 Copyright:
+            <a className="text-footer-web" href="https://4geeksacademy.com/">
+              4Geeks Academy
+            </a>
+          </div>
+        </footer>
+      </section>
+    </div>
+  );
+};
