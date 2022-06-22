08debc4c2b096853b22f274041e4c859c179705d
Phherten <phigueraherten@gmail.com>
2022-06-22 10:10:38 +0000

    cards modificadas

--- src/front/js/component/navbarFondo.js
+++ src/front/js/component/navbarFondo.js
@@ -109,7 +109,7 @@ export const NavbarFondo = () => {
                             class="dropdown-item navbar-button-user-login dropdown-text pb-3"
                             type="button"
                           >
-                            Registrarse
+                            <p className="text-drop">Registrarse</p>
                           </button>
                         </Link>
                       </li>
