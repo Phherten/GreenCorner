94c97720c59752199e245738e9ee121267f32b5e
Orealba <orealbas@gmail.com>
2022-06-22 15:48:49 +0000

    creación de componentes

---
+++ src/front/js/component/headerInfoPlantas.js
@@ -0,0 +1,8 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+
+import "../../styles/headerInfoPlantas.css";
+
+export const HeaderInfoPlantas = () => {
+  return <div></div>;
+};
---
+++ src/front/js/pages/cuidadosPlantas.js
@@ -0,0 +1,9 @@
+import React, { useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
+import { headerInfoPlantas } from "../component/headerInfoPlantas";
+
+import { Card_plagas } from "../component/card_plagas";
+
+export const CuidadosPlantas = () => {
+  return <headerInfoPlantas />;
+};
