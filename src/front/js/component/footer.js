485a60040169cff2572c61e7201327323548b8ff
barbosa92 <alexrp_92@hotmail.com>
2022-08-13 18:28:34 +0000

    Footer modificado incluye 3 secciones

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import "../../styles/footer.css";
 import logoFooter from "../../img/Logo_letras_amarillo.png";
 
-
 export const Footer = () => {
   return (
     <div className="container-fluid footer-home">
@@ -11,18 +10,19 @@ export const Footer = () => {
           <div className="container p-4 pb-0">
             <section className="">
               <div className="d-flex justify-content-center align-items-center">
-                <img className="header-logoLetrasAmarillo" src={logoFooter} />
+                {/* <img className="header-logoLetrasAmarillo" src={logoFooter} /> */}
               </div>
             </section>
           </div>
 
-          <div className="text-center p-3 footer-text">
-            © 2020 Copyright:
-            <a className="text-footer-web" href="https://4geeksacademy.com/">
-              4Geeks Academy
-            </a>
-          </div>
-          <section className="mb-4 text-center">
+          <section className="enlaces-footer">
+            <a className="texto-enlaces">Contacto</a>
+            <a className="texto-enlaces">Política de privacidad</a>
+            <a className="texto-enlaces">Acerca de</a>
+          </section>
+          <hr></hr>
+
+          <section className="logos mb-4 text-center">
             <a
               className="btn btn-outline-light btn-floating m-1"
               href="#!"
@@ -55,9 +55,15 @@ export const Footer = () => {
               <i className="fab fa-linkedin-in"></i>
             </a>
           </section>
+          <div className="text-center p-3 footer-text">
+            © 2022 Copyright:
+            <a className="text-footer-web" href="https://4geeksacademy.com/">
+              Green Corner
+            </a>
+          </div>
+          {/* logos RRHH */}
         </footer>
       </section>
     </div>
   );
 };
-
