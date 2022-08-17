2621c41d51fb05db197a12c4ba252a54cbb7c531
Phherten <phigueraherten@gmail.com>
2022-08-17 18:02:39 +0000

    tiempo espera arreglado no lo se

--- src/front/js/pages/privada.js
+++ src/front/js/pages/privada.js
@@ -16,11 +16,13 @@ export const Privada = () => {
   const [load, setLoad] = useState(false);
 
   useEffect(() => {
-    const result = actions.privado();
-    if (result) {
-      actions.getPlantsUser();
+    setTimeout(() => {
+      const result = actions.privado();
+      if (result) {
+        actions.getPlantsUser();
+      }
       setLoad(true);
-    }
+    }, 3000);
   }, []);
 
   useEffect(() => {
